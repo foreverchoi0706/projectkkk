4339f74ca58e89cbee9b8d9e22b53c251810fc45
최영원 <57241273+foreverchoi0706@users.noreply.github.com>
2024-09-12 12:10:05 +0000

    improve code

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,6 +1,15 @@
 "use client";
 import useAuth from "@/hooks/useAuth.ts";
-import { INVALILD_FORMAT_EMAIL, INVALILD_FORMAT_PASSWORD, INVALILD_FORMAT_PHONE, INVALILD_REPASWORD, REQUIRED_EMAIL, REQUIRED_NAME, REQUIRED_PASSWORD, REQUIRED_PHONE } from "@/utils/constants";
+import {
+  INVALILD_FORMAT_EMAIL,
+  INVALILD_FORMAT_PASSWORD,
+  INVALILD_FORMAT_PHONE,
+  INVALILD_REPASWORD,
+  REQUIRED_EMAIL,
+  REQUIRED_NAME,
+  REQUIRED_PASSWORD,
+  REQUIRED_PHONE,
+} from "@/utils/constants";
 import { axiosInstance } from "@/utils/queryKeys.ts";
 import { IResponse, ISignUpParams, IUserInfo, TError } from "@/utils/types.ts";
 import { useMutation } from "@tanstack/react-query";
@@ -12,13 +21,8 @@ import { useNavigate } from "react-router-dom";
 const Page: FC = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
-  const signUpMutation = useMutation<
-    AxiosResponse<IResponse<IUserInfo>>,
-    TError,
-    ISignUpParams
-  >({
-    mutationFn: (signUpParams: ISignUpParams) =>
-      axiosInstance.post("/member/join", signUpParams),
+  const signUpMutation = useMutation<AxiosResponse<IResponse<IUserInfo>>, TError, ISignUpParams>({
+    mutationFn: (signUpParams: ISignUpParams) => axiosInstance.post("/member/join", signUpParams),
     onSuccess: ({ data }) => login(data.result),
     onError: ({ responseMessage }) => alert(responseMessage),
   });
@@ -38,10 +42,16 @@ const Page: FC = () => {
     >
       <Typography.Title>회원가입</Typography.Title>
       <Form onFinish={onFinish} autoComplete="off">
-        <Form.Item<ISignUpParams> name="email" rules={[{ required: true, message: REQUIRED_EMAIL }, {
-          type: 'email',
-          message: INVALILD_FORMAT_EMAIL,
-        }]}>
+        <Form.Item<ISignUpParams>
+          name="email"
+          rules={[
+            { required: true, message: REQUIRED_EMAIL },
+            {
+              type: "email",
+              message: INVALILD_FORMAT_EMAIL,
+            },
+          ]}
+        >
           <Input placeholder={REQUIRED_EMAIL} />
         </Form.Item>
 
@@ -49,29 +59,49 @@ const Page: FC = () => {
           <Input placeholder={REQUIRED_NAME} />
         </Form.Item>
 
-        <Form.Item<ISignUpParams> name="phone" rules={[{ required: true, message: REQUIRED_PHONE }, {
-          pattern: /^01[0-9][0-9]{8,9}$/,
-          message: INVALILD_FORMAT_PHONE,
-        },]}>
+        <Form.Item<ISignUpParams>
+          name="phone"
+          rules={[
+            { required: true, message: REQUIRED_PHONE },
+            {
+              pattern: /^01[0-9][0-9]{8,9}$/,
+              message: INVALILD_FORMAT_PHONE,
+            },
+          ]}
+        >
           <Input type="number" placeholder={REQUIRED_PHONE} />
         </Form.Item>
 
-        <Form.Item<ISignUpParams> name="password" rules={[{ required: true, message: REQUIRED_PASSWORD }, {
-          pattern: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
-          message: INVALILD_FORMAT_PASSWORD,
-        }]}>
+        <Form.Item<ISignUpParams>
+          name="password"
+          rules={[
+            { required: true, message: REQUIRED_PASSWORD },
+            {
+              pattern: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
+              message: INVALILD_FORMAT_PASSWORD,
+            },
+          ]}
+        >
           <Input.Password placeholder={REQUIRED_PASSWORD} />
         </Form.Item>
 
-        <Form.Item<ISignUpParams> name="rePassword" dependencies={['password']} rules={[{ required: true, message: REQUIRED_PASSWORD }, {
-          pattern: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
-          message: INVALILD_FORMAT_PASSWORD,
-        }, ({ getFieldValue }) => ({
-          validator(_, value) {
-            if (!value || getFieldValue('password') === value) return Promise.resolve();
-            return Promise.reject(INVALILD_REPASWORD);
-          },
-        }),]}>
+        <Form.Item<ISignUpParams>
+          name="passwordConfirm"
+          dependencies={["password"]}
+          rules={[
+            { required: true, message: REQUIRED_PASSWORD },
+            {
+              pattern: /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
+              message: INVALILD_FORMAT_PASSWORD,
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) return Promise.resolve();
+                return Promise.reject(INVALILD_REPASWORD);
+              },
+            }),
+          ]}
+        >
           <Input.Password placeholder={REQUIRED_PASSWORD} />
         </Form.Item>
 
