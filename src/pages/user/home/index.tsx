657cb8e29df71531fcb4522b967660eeed0dba8f
foreverchoi <foreverchoi0706@gmail.com>
2024-10-09 22:46:51 +0900

    improve code

--- src/pages/user/home/index.tsx
+++ src/pages/user/home/index.tsx
@@ -1,22 +1,42 @@
-import { Carousel, Flex } from "antd";
+import { UnorderedListOutlined } from "@ant-design/icons";
+import { Button, Carousel, Flex } from "antd";
 import { FC } from "react";
 
 const Page: FC = () => {
   return (
     <main>
-      <Flex className="flex-col">
+      <Flex className="flex-col gap-4">
         <Carousel>
-          <div>dasds</div>
-          <div>dasds</div>
-          <div>dasds</div>
+          <img
+            alt="img"
+            src="https://cf.image-farm.s.zigzag.kr/original/cms/2024/09/27/202409270520214482_061783.jpg?width=1029&height=1029&quality=100&format=webp&transparent=true"
+          />
+          <img
+            alt="img"
+            src="https://cf.image-farm.s.zigzag.kr/original/cms/2024/10/02/202410021202530837_041069.jpg"
+          />
+          <img
+            alt="img"
+            src="https://cf.image-farm.s.zigzag.kr/original/cms/2024/10/04/202410040917163324_093565.jpg"
+          />
+          <img
+            alt="img"
+            src="https://cf.image-farm.s.zigzag.kr/original/cms/2024/10/02/202410021202530837_041069.jpg"
+          />
         </Carousel>
-        <Flex className="gap-4">
-          <div>카테고리</div>
-          <div>👖</div>
-          <div>👖</div>
-          <div>👖</div>
-          <div>👖</div>
-          <div>👖</div>
+        <Flex className="gap-4 overflow-x-auto">
+          <Button>
+            <UnorderedListOutlined />
+          </Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
+          <Button>👖</Button>
         </Flex>
       </Flex>
     </main>
