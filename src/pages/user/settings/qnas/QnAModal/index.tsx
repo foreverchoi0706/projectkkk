42d912d78d1a32a55b623cdf3f42bc4ee1eadaa4
foreverchoi <foreverchoi0706@gmail.com>
2024-11-18 20:22:28 +0900

    improve code

--- src/pages/user/settings/qnas/QnAModal/index.tsx
+++ src/pages/user/settings/qnas/QnAModal/index.tsx
@@ -1,7 +1,6 @@
 import user from "@/queryKeys/user";
 import { useQuery } from "@tanstack/react-query";
-import { Flex, Modal, ModalProps, Typography } from "antd";
-import TextArea from "antd/es/input/TextArea";
+import { Modal, ModalProps } from "antd";
 import { FC } from "react";
 
 const QnAModal: FC<ModalProps & { id: number }> = ({ id, ...rest }) => {
@@ -13,22 +12,22 @@ const QnAModal: FC<ModalProps & { id: number }> = ({ id, ...rest }) => {
 
   return (
     <Modal {...rest}>
-      <Flex className="flex-col gap-4">
-        <Typography className="text-center font-bold text-2xl">{qna.subject}</Typography>
-        <TextArea className="resize-none" readOnly defaultValue={qna.description} />
-        {qna.answerStatus === "WAITING" ? (
-          <Flex className="flex-col justify-center items-center gap-8 p-8">
-            <Typography className="text-center font-bold text-2xl">
-              아직 관리자가 답변을 달지 않았어요 조금만 더 기다려주세요
-            </Typography>
-          </Flex>
-        ) : (
-          <Flex>
-            <Typography>{qna.answerDate}</Typography>
-            <TextArea readOnly />
-          </Flex>
-        )}
-      </Flex>
+      {/*<Flex className="flex-col gap-4">*/}
+      {/*  <Typography className="text-center font-bold text-2xl">{qna.subject}</Typography>*/}
+      {/*  <TextArea className="resize-none" readOnly defaultValue={qna.description} />*/}
+      {/*  {qna.answerStatus === "WAITING" ? (*/}
+      {/*    <Flex className="flex-col justify-center items-center gap-8 p-8">*/}
+      {/*      <Typography className="text-center font-bold text-2xl">*/}
+      {/*        아직 관리자가 답변을 달지 않았어요 조금만 더 기다려주세요*/}
+      {/*      </Typography>*/}
+      {/*    </Flex>*/}
+      {/*  ) : (*/}
+      {/*    <Flex>*/}
+      {/*      <Typography>{qna.answerDate}</Typography>*/}
+      {/*      <TextArea readOnly />*/}
+      {/*    </Flex>*/}
+      {/*  )}*/}
+      {/*</Flex>*/}
     </Modal>
   );
 };
