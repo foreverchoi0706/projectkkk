c8e12e3ef5ee4e497606e76aaa8d4b7de1a0fd7c
foreverchoi <foreverchoi0706@gmail.com>
2024-12-07 16:14:16 +0900

    improve code

--- src/hooks/useLike.ts
+++ src/hooks/useLike.ts
@@ -3,8 +3,10 @@ import axiosInstance from "@/utils/axiosInstance.ts";
 import type { TError } from "@/utils/types.ts";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const useLike = (initialLiked: boolean, id?: number) => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const [isLiked, setIsLiked] = useState<boolean>(initialLiked);
 
@@ -12,14 +14,20 @@ const useLike = (initialLiked: boolean, id?: number) => {
     mutationFn: () => axiosInstance.post(`/wishList/add?productId=${id}`),
     onSuccess: () =>
       queryClient.invalidateQueries(user.products.wish()).then(() => setIsLiked(!isLiked)),
-    onError: ({ responseMessage }) => alert(responseMessage),
+    onError: ({ responseMessage }) => {
+      alert(responseMessage);
+      navigate("/signin");
+    },
   });
 
   const unlikeMutation = useMutation<unknown, TError>({
     mutationFn: () => axiosInstance.delete(`/wishList/remove?productId=${id}`),
     onSuccess: () =>
       queryClient.invalidateQueries(user.products.wish()).then(() => setIsLiked(!isLiked)),
-    onError: ({ responseMessage }) => alert(responseMessage),
+    onError: ({ responseMessage }) => {
+      alert(responseMessage);
+      navigate("/signin");
+    },
   });
 
   useEffect(() => {
