745f8813440bbdd6eba75c7b4aebcb5f34e52370
foreverchoi <foreverchoi0706@gmail.com>
2024-12-07 15:51:45 +0900

    improve code

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,6 +2,7 @@ import { ACCESS_TOKEN } from "@/utils/constants.ts";
 import { getCookie } from "@/utils/cookie.ts";
 import {
   BellOutlined,
+  BellTwoTone,
   HeartFilled,
   HeartOutlined,
   HomeFilled,
@@ -15,15 +16,18 @@ import {
   UnorderedListOutlined,
 } from "@ant-design/icons";
 import { CompatClient, Stomp } from "@stomp/stompjs";
-import { Button, Flex, Layout, Typography } from "antd";
-import { FC, useEffect, useState } from "react";
+import { Button, Flex, Layout, Tour, TourProps, Typography } from "antd";
+import { FC, useEffect, useRef, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Link, Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import SockJS from "sockjs-client";
 
 const User: FC = () => {
   const { pathname } = useLocation();
+  const refNotificationBell = useRef<HTMLSpanElement>(null);
   const [client, setClient] = useState<CompatClient | null>(null);
+  const [notifications, setNotifications] = useState<unknown[]>([]);
+  const [isOpenNotificationTour, setOpenNotificationTour] = useState<boolean>(false);
 
   useEffect(() => {
     const accessToken = getCookie(ACCESS_TOKEN);
@@ -37,7 +41,7 @@ const User: FC = () => {
       { Authorization: `Bearer ${accessToken}` },
       () => {
         stompClient.subscribe("/user/queue/notifications", (message) => {
-          console.log(message.body);
+          setNotifications(JSON.parse(message.body));
         });
       },
       console.error,
@@ -48,6 +52,26 @@ const User: FC = () => {
     return () => client?.disconnect();
   }, []);
 
+  const steps: TourProps["steps"] = [
+    {
+      title: "",
+      cover: notifications.length ? (
+        <ul className="flex flex-col gap-4">
+          {notifications.map(() => (
+            <li className="bg-white text-black px-4 py-2 rounded">알림</li>
+          ))}
+        </ul>
+      ) : (
+        <div>알림이 없습니다</div>
+      ),
+      target: refNotificationBell.current || null,
+      closeIcon: false,
+      nextButtonProps: {
+        children: "닫기",
+      },
+    },
+  ];
+
   return (
     <Layout className="bg-white relative my-0 mx-auto max-w-[600px] pb-[78px]">
       <Flex className="justify-between p-4">
@@ -55,10 +79,30 @@ const User: FC = () => {
           <Typography className="text-2xl font-bold flex-shrink-0">KKK</Typography>
         </Link>
         <Flex className="gap-4">
-          <BellOutlined
-            className="text-2xl"
-            onClick={() => client?.send("/app/log", {}, "Test log messagdasdasdase")}
-          />
+          <>
+            {notifications.length > 0 ? (
+              <BellTwoTone
+                onClick={() => setOpenNotificationTour(true)}
+                className="text-2xl"
+                ref={refNotificationBell}
+              />
+            ) : (
+              <BellOutlined
+                ref={refNotificationBell}
+                className="text-2xl"
+                onClick={() => setOpenNotificationTour(true)}
+                // onClick={() => client?.send("/app/log", {}, "Test log messagdasdasdase")}
+              />
+            )}
+            <Tour
+              open={isOpenNotificationTour}
+              onClose={() => setOpenNotificationTour(false)}
+              mask={false}
+              type="primary"
+              steps={steps}
+              animated={true}
+            />
+          </>
           <Link to="/search" className="flex items-center">
             <SearchOutlined className="text-2xl" />
           </Link>
