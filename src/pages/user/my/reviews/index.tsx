7b83fd8392a22f7dd12161d345ae80f5cce47191
최영원 <57241273+foreverchoi0706@users.noreply.github.com>
2024-12-13 02:02:20 +0000

    improve code

--- src/pages/user/my/reviews/index.tsx
+++ src/pages/user/my/reviews/index.tsx
@@ -1,44 +1,19 @@
 import user from "@/queryKeys/user";
 import { useQuery } from "@tanstack/react-query";
-import { Collapse, Flex, Input, type InputRef, Typography, Image } from "antd";
-import { type ChangeEvent, type FC, type KeyboardEventHandler, useEffect, useRef } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { debounceTime, distinctUntilChanged, fromEvent, map } from "rxjs";
+import { Collapse, Flex, Typography, Image } from "antd";
+import { type FC } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Page: FC = () => {
-  const navigate = useNavigate();
-  const refInput = useRef<InputRef | null>(null);
   const [searchParams] = useSearchParams({ size: "15", page: "1" });
 
-  const onKeyDownSearch: KeyboardEventHandler<HTMLInputElement> = ({
-    key,
-    currentTarget: { value },
-  }) => {
-    if (key !== "Enter") return;
-    searchParams.set("keyword", value);
-    navigate(`/my/coupons?${searchParams.toString()}`);
-  };
-
   const { data: reviews } = useQuery(user.reviews.all(searchParams.toString()));
-
-  useEffect(() => {
-    if (!refInput.current?.input) return;
-    const subscription = fromEvent<ChangeEvent<HTMLInputElement>>(refInput.current.input, "input")
-      .pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        map(({ target }) => target.value),
-      )
-      .subscribe();
-    return () => subscription.unsubscribe();
-  }, []);
 
   if (!reviews) return null;
 
   return (
     <main className="h-full">
       <Flex className="h-full flex-col gap-4">
-        <Input ref={refInput} placeholder="리뷰를 검색해보세요" onKeyDown={onKeyDownSearch} />
         <Flex className="gap-4 flex-col flex-grow">
           {reviews.content.length > 0 ? (
             <Collapse
