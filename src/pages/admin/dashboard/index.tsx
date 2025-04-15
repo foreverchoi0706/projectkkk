40dc558bb3cc5af9f26758378d3962307164ee9a
foreverchoi0706 <foreverchoi0706@saramin.co.kr>
2025-04-15 09:45:24 +0900

    improve code

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -6,7 +6,7 @@ const Page: FC = () => {
       title="dashboard"
       width="100%"
       height="100%"
-      src="https://www.projectkkk.com:5601/login?next=%2Fapp%2Fdashboards#/view/c72af0a0-e217-11ef-bdd7-453347394a48?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-15w,to:now))"
+      src="https://www.projectkkk.com:5601/app/dashboards#/view/b885cd80-0fd6-11f0-b9da-41f9d64eebe5?_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))"
     />
   );
 };
